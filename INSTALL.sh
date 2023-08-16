aed34eb1f21f51d7fa74e28f25c5f8a4d15f5827
Vitalij Gotovskij <vitalij@cdeveloper.eu>
2023-08-16 12:48:58 +0300

    Added possibility to install command_extensions.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,63 +1,63 @@
-#!/bin/bash
+Certainly! Below is the updated installation script which prompts the user if they want to install the extension (`command_extensions.sh`) in addition to the main script (`cd++.sh`):
 
+```bash
+#!/bin/bash
 
 CURRENT_DIR=$(dirname "$0")
 SCRIPTS_PATH="${CURRENT_DIR}/scripts"
-SCRIPT_NAME="cd++.sh"
-QUICK_JUMP_SCRIPT_PATH="${SCRIPTS_PATH}/${SCRIPT_NAME}"
+MAIN_SCRIPT_NAME="cd++.sh"
+EXTENSION_SCRIPT_NAME="command_extensions.sh"
+MAIN_SCRIPT_PATH="${SCRIPTS_PATH}/${MAIN_SCRIPT_NAME}"
+EXTENSION_SCRIPT_PATH="${SCRIPTS_PATH}/${EXTENSION_SCRIPT_NAME}"
 TARGET_DIR="/usr/local/bin"
 
+# Function to append to the appropriate shell configuration file
+function append_to_shell_config {
+    local script_path="$1"
+    case "$SHELL" in
+        *bash*)
+            echo ". $script_path" >> ~/.bashrc
+            ;;
+        *zsh*)
+            echo ". $script_path" >> ~/.zshrc
+            ;;
+        *fish*)
+            echo "source $script_path" >> ~/.config/fish/config.fish
+            ;;
+        *csh*)
+            echo "source $script_path" >> ~/.cshrc
+            ;;
+        *tcsh*)
+            echo "source $script_path" >> ~/.tcshrc
+            ;;
+        *ksh*)
+            echo ". $script_path" >> ~/.kshrc
+            ;;
+        # Add other shells here if needed
+        *)
+            echo "Unrecognized shell. Please add the script to your shell's configuration file manually."
+            return 1
+            ;;
+    esac
+}
 
-cp "$QUICK_JUMP_SCRIPT_PATH" "$TARGET_DIR/"
-chmod +x "$TARGET_DIR/$SCRIPT_NAME"
+# Install main script
+cp "$MAIN_SCRIPT_PATH" "$TARGET_DIR/"
+chmod +x "$TARGET_DIR/$MAIN_SCRIPT_NAME"
+append_to_shell_config "$TARGET_DIR/$MAIN_SCRIPT_NAME"
 
-
-if [[ "$SHELL" == *"bash"* ]]; then
-    echo ". $TARGET_DIR/$SCRIPT_NAME" >> ~/.bashrc
-
-elif [[ "$SHELL" == *"zsh"* ]]; then
-    echo ". $TARGET_DIR/$SCRIPT_NAME" >> ~/.zshrc
-
-elif [[ "$SHELL" == *"fish"* ]]; then
-    echo "source $TARGET_DIR/$SCRIPT_NAME" >> ~/.config/fish/config.fish
-
-elif [[ "$SHELL" == *"csh"* ]]; then
-    echo "source $TARGET_DIR/$SCRIPT_NAME" >> ~/.cshrc
-
-elif [[ "$SHELL" == *"tcsh"* ]]; then
-    echo "source $TARGET_DIR/$SCRIPT_NAME" >> ~/.tcshrc
-
-elif [[ "$SHELL" == *"ksh"* ]]; then
-    echo ". $TARGET_DIR/$SCRIPT_NAME" >> ~/.kshrc
-
-# Add other shells here if needed
-
+# Ask user about installing extension
+read -p "Do you want to install the command extensions as well? (yes/NO): " user_input
+if [[ "$user_input" == "yes" ]]; then
+    cp "$EXTENSION_SCRIPT_PATH" "$TARGET_DIR/"
+    chmod +x "$TARGET_DIR/$EXTENSION_SCRIPT_NAME"
+    append_to_shell_config "$TARGET_DIR/$EXTENSION_SCRIPT_NAME"
+    echo "Command extensions successfully installed!"
 else
-    echo "Unrecognized shell. Please add the script to your shell's configuration file manually."
+    echo "Command extensions will not be installed"
 fi
 
 echo "Installation complete!"
+```
 
-
-# # Function to create and link new script
-# # Accepts two parameters: 
-# #   1. The command name (e.g., go, mark, list_dirs)
-# #   2. The function name to call within the script (e.g., go, mark, list_dirs)
-# create_and_link_script() {
-#     COMMAND_NAME=$1
-#     FUNCTION_NAME=$2
-
-#     COMMAND_PATH="${BIN_PATH}/${COMMAND_NAME}"
-
-#     echo "#!/bin/bash
-# source $QUICK_JUMP_SCRIPT_PATH
-# $FUNCTION_NAME \"\$@\"
-# " > $COMMAND_PATH
-
-#     chmod +x $COMMAND_PATH
-# }
-
-# # Create and link scripts for the go, mark, and list_dirs commands
-# create_and_link_script "go" "go"
-# create_and_link_script "mark" "mark"
-# create_and_link_script "list_dirs" "list_dirs"
+In this script, I've added a `read` command to prompt the user for input. If the user inputs "yes", it will install the `command_extensions.sh` script the same way as the `cd++.sh` script. If the user presses "enter" or provides any input other than "yes", the extensions won't be installed.
