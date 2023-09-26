31f51f8c446567eb6cfaa4e1be7273dcc4745495
Vitaly Gotovski <1889621+vitalij555@users.noreply.github.com>
2023-09-26 20:02:59 +0300

    Bug fixes in  command_extensions.sh

--- scripts/command_extensions.sh
+++ scripts/command_extensions.sh
@@ -5,7 +5,14 @@ function resolve_label {
         return 1
     fi
 
-    local dir=$(grep "^$1 " ~/.dirs | cut -d' ' -f2-)
+    #echo "Checking ig $HOME/.dirs exist..."
+    if [ ! -f "$HOME/.dirs" ]
+    then
+        #echo "Does not exist  - creating"
+        touch "$HOME/.dirs"
+    fi
+
+    local dir=$(grep "^$1 " "$HOME/.dirs" | cut -d' ' -f2-)
     if [ -z "$dir" ]; then
         return 1
     fi
@@ -25,136 +32,162 @@ function resolve_path_or_label {
 }
 
 function process_args {
-    local -n _args_out=$1
+    local _args_out=$1
     shift
     for arg in "$@"
     do
-        if [[ $arg == \#* && $arg != \\#* ]]; then
-            local label_name="${arg#\#}"   # Remove the '#' at the beginning
+        #printf "\nCurrent arg is {%s}" "$arg" 1>&2 
+        if [[ $arg == \@* && $arg != \\@* ]]; then
+            local label_name="${arg#\@}"   # Remove the '@' at the beginning
+            llocal path_suffix=""
+
+            if [[ $label_name == */* ]]; then  # ensure we are taking something only if path slash ws found
+                path_suffix="${label_name#*/}" # overwise keep path_suffix empty
+            fi
+
+            label_name="${label_name%%/*}" # take only until first path slash
             local resolved_path=$(resolve_label "$label_name")
 
+            printf "\nlabel_name: {%s}   resolved_path: {%s}" "$label_name" "$resolved_path" 1>&2 
             if [ -n "$resolved_path" ]; then
-                _args_out+=("$resolved_path")
+                eval "$_args_out+=(\"$resolved_path/$path_suffix\")"
             else
-                _args_out+=("$arg")
+                eval "$_args_out+=(\"$arg\")"
             fi
         else
-            _args_out+=("$arg")
+            eval "$_args_out+=(\"$arg\")"
         fi
     done
 }
 
 
-function cd {
-    local path="$(resolve_path_or_label "$1")"
-    builtin cd "$path"
-}
+# function cd {
+#     local path="$(resolve_path_or_label "$1")"
+#     command cd "$path"
+# }
 
 function cp {
+    #printf "\ncp called" 1>&2 
     local args=()
     process_args args "$@"
-    builtin cp "${args[@]}"
+    command cp "${args[@]}"
 }
 
 function mv {
+    #printf "\nmv called" 1>&2
     local args=()
     process_args args "$@"
-    builtin mv "${args[@]}"
+    command mv "${args[@]}"
 }
 
 function mkdir {
+    #printf "\nmkdir called" 1>&2
     local args=()
     process_args args "$@"
-    builtin mkdir "${args[@]}"
+    command mkdir "${args[@]}"
 }
 
 function rmdir {
+    #printf "\nrmdir called" 1>&2
     local args=()
     process_args args "$@"
-    builtin rmdir "${args[@]}"
+    command rmdir "${args[@]}"
 }
 
 function rm {
+    #printf "\nrm called" 1>&2
     local args=()
     process_args args "$@"
-    builtin rm "${args[@]}"
+    command rm "${args[@]}"
 }
 
 function ln {
+    #printf "\nln called" 1>&2
     local args=()
     process_args args "$@"
-    builtin ln "${args[@]}"
+    command ln "${args[@]}"
 }
 
 function chmod {
+    #printf "\nchmod called" 1>&2
     local args=()
     process_args args "$@"
-    builtin chmod "${args[@]}"
+    command chmod "${args[@]}"
 }
 
 function chown {
+    #printf "\nchown called" 1>&2
     local args=()
     process_args args "$@"
-    builtin chown "${args[@]}"
+    command chown "${args[@]}"
 }
 
 function ls {
+    #printf "\nls called" 1>&2
     local args=()
     process_args args "$@"
-    builtin ls "${args[@]}"
+    command ls "${args[@]}"
 }
 
 function touch {
+    #printf "\ntouch called" 1>&2
     local args=()
     process_args args "$@"
-    builtin touch "${args[@]}"
+    command touch "${args[@]}"
 }
 
 function cat {
+    #printf "\ncat called" 1>&2
     local args=()
     process_args args "$@"
-    builtin cat "${args[@]}"
+    command cat "${args[@]}"
 }
 
 function grep {
+    #printf "\ngrep called" 1>&2
     local args=()
     process_args args "$@"
-    builtin grep "${args[@]}"
+    command grep "${args[@]}"
 }
 
 function find {
+    #printf "\nfind called" 1>&2
     local args=()
     process_args args "$@"
-    builtin find "${args[@]}"
+    command find "${args[@]}"
 }
 
-function echo {
-    local args=()
-    process_args args "$@"
-    builtin echo "${args[@]}"
-}
+# function echo {
+#     local args=()
+#     process_args args "$@"
+#     command echo "${args[@]}"
+# }
 
 function diff {
+    #printf "\ndiff called" 1>&2
     local args=()
     process_args args "$@"
-    builtin diff "${args[@]}"
+    command diff "${args[@]}"
 }
 
 function tail {
+    #printf "\ntail called" 1>&2
     local args=()
     process_args args "$@"
-    builtin tail "${args[@]}"
+    command tail "${args[@]}"
 }
 
 function head {
+    #printf "\nhead called" 1>&2
     local args=()
     process_args args "$@"
-    builtin head "${args[@]}"
+    command head "${args[@]}"
 }
 
 function less {
+    #printf "\nless called" 1>&2
     local args=()
     process_args args "$@"
-    builtin less "${args[@]}"
+    command less "${args[@]}"
 }
 
